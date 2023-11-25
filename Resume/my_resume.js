f110fcda5dcf823ab45bb9e98424e273f46b3201
Anil Yadav <138858484+AnilYadav8421@users.noreply.github.com>
2023-11-25 11:21:14 +0530

    Add files via upload

---
+++ Resume/my_resume.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('.form')
+
+let inputShow = true
+function toggle(){
+    if(inputShow){
+        form.style.display = "none"
+        inputShow = false
+    }else{
+
+    }
+}
